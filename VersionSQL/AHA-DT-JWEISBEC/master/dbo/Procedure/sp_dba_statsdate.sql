cb4791cc0eba837510b9f0dd12252f9dfdf6c7b5
sqlgumbo <unknown>
2018-07-16 17:33:48 -0500

    adding semicolon

--- VersionSQL/AHA-DT-JWEISBEC/master/dbo/Procedure/sp_dba_statsdate.sql
+++ VersionSQL/AHA-DT-JWEISBEC/master/dbo/Procedure/sp_dba_statsdate.sql
@@ -1,16 +1,17 @@
 /****** Object:  Procedure [dbo].[sp_dba_statsdate]    Committed by VersionSQL https://www.versionsql.com ******/
 
 
+
 CREATE PROCEDURE  [dbo].[sp_dba_statsdate]   @table   VARCHAR(120) = NULL
 AS
 BEGIN
-   SET NOCOUNT ON
+   SET NOCOUNT ON;
    --
    SELECT (object_name(si.id))   		AS 'Table Name',
            si.name				AS 'Index',
            STATS_DATE(si.id, si.indid)		AS 'Statistics Date'
    FROM sysindexes si
    WHERE OBJECT_NAME(si.id)  =  ISNULL(@table, OBJECT_NAME(si.id))
-   ORDER BY 'Table Name'
+   ORDER BY 'Table Name';
    --
 END
